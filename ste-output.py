a78f9891a27cce79c560e924cb5fb3d4ba415e01
tomaszkow <tomaszkow@gmail.com>
2012-01-06 20:45:18 +0100

    Less memory needed. Renamed some variables. - Make use of a buffered reader for reading small portions of the input file when needed - Most variables have a bit longer names.

--- ste-output.py
+++ ste-output.py
@@ -126,51 +126,61 @@ class STE_StressExtractor:
         t0 = time.time()
 
         logging.info("Reading header...")
-        data = self._ste_input.read(-1) #read whole file
+        data = self._ste_input.read(84)
+        nr_of_elements = struct.unpack_from('i',data,12)[0]
+        nr_of_nodes = struct.unpack_from('i',data,36)[0]
+        data_size = struct.unpack_from('i',data,28)[0] # 4B numbers count
+        data_start = struct.unpack_from('i',data,80)[0]
 
-        elem_no = struct.unpack('i',data[12:16])[0]
-        node_no = struct.unpack('i',data[36:40])[0]
-        data_size = struct.unpack('i',data[28:32])[0] # ilosc 4-bajtowych liczb
-        data_start = struct.unpack('i',data[80:84])[0]
-        byte_no = int(struct.unpack('f',data[data_start+4:data_start+8])[0])
-        mesh_type = int(struct.unpack('f',data[data_start+12:data_start+16])[0])
-        logging.debug('Calculation element No.: %i' % (elem_no))
-        logging.debug('Calculation node No.: %i' % (node_no)) #, data_size, data_start, byte_no, mesh_type
-        elem_no = int(data_size / 189)
-        logging.debug('Real elem No: %i' % (elem_no))
+        # all elements have the same number of nodes
+        # the size of a record holding a single element with it's nodes is constant
+        self._ste_input.seek(data_start)
+        data = self._ste_input.peek(16)
+        record_size = int(struct.unpack_from('f',data,4)[0])
+        nodes_per_element = int(struct.unpack_from('f',data,12)[0])
 
-        start = data_start
-        mpa = 1000000
-        output_node =[[0]*7 for i in range(node_no)]
-        real_node_no = 0.0
+        logging.debug('Expecting %i elements' % (nr_of_elements))
+        logging.debug('Expecting %i nodes' % (nr_of_nodes)) 
+        nr_of_elements = int(data_size / 189)
+        logging.debug('Real elem No: %i' % (nr_of_elements))
+
+        mpa = 1000000 # 1MPa = 10**6 Pa
+        nodes =[[0]*7 for i in xrange(nr_of_nodes)]
+        real_nr_of_nodes = 0.0
 
         logging.info("Extracting stress values. Elements are saved. Nodes are computed. (It takes a while)...")
-        for i in xrange(0, elem_no):
+        for i in xrange(0, nr_of_elements):
+            data = self._ste_input.read(4*record_size)
            
-            elem = int(struct.unpack('f',data[start:start+4])[0])
-            dat = struct.unpack('6f',data[start+56:start+80])
-            self._save_element(elem, (dat[0]/mpa, dat[1]/mpa, dat[2]/mpa, dat[3]/mpa, dat[4]/mpa, dat[5]/mpa) )
+            # extracting stress values for an element
+            element_no = int(struct.unpack_from('f',data,0)[0])
+            element_stress = struct.unpack_from('6f',data,56)
+            self._save_element(element_no, (element_stress[0]/mpa, element_stress[1]/mpa, 
+                                            element_stress[2]/mpa, element_stress[3]/mpa,  
+                                            element_stress[4]/mpa, element_stress[5]/mpa))
             
-            nod_dat = struct.unpack(str(mesh_type*8)+'f',data[start+116:start+116+mesh_type*32])
+            # extracting stress values for nodes in this element
+            raw_nodes = struct.unpack_from(str(nodes_per_element*8)+'f',data,116)
             
-            for k in range (0, mesh_type):
-                int_val = int(nod_dat[0+8*k])
-                if real_node_no < int_val:
-                    real_node_no = int_val
-                    
-                nod = output_node[int_val-1]
-                nod[0] = nod[0] +1
-                
-                for j in range(1,7):
-                    nod[j] = nod[j]+ nod_dat[j+8*k]
+            for node_in_element_no in range (0, nodes_per_element):
+                node_no = int(raw_nodes[8*node_in_element_no])
+                if real_nr_of_nodes < node_no:
+                    real_nr_of_nodes = node_no
 
-            start = start + 4 * byte_no
+                n = nodes[node_no-1] # stress data for this node found so far
+                n[0] += 1 # counting elements that share this node
+                
+                # summing up stress values for this node
+                for j in xrange(1,7):
+                    n[j] += raw_nodes[j+8*node_in_element_no]
 
         logging.info("Saving stress values for nodes...")
-        logging.debug('Real node No: %i' % (real_node_no))
-        for i in range(real_node_no):
-            nod = output_node[i]
-            self._save_node(i+1, (nod[1]/nod[0]/mpa, nod[2]/nod[0]/mpa, nod[3]/nod[0]/mpa, nod[4]/nod[0]/mpa, nod[5]/nod[0]/mpa, nod[6]/nod[0]/mpa))
+        logging.debug('Real node No: %i' % (real_nr_of_nodes))
+        # dividing stress values for a node by number of elements that share this node
+        for node_no in range(real_nr_of_nodes):
+            n = nodes[node_no]
+            self._save_node(node_no+1, (n[1]/n[0]/mpa, n[2]/n[0]/mpa, n[3]/n[0]/mpa, 
+                                        n[4]/n[0]/mpa, n[5]/n[0]/mpa, n[6]/n[0]/mpa))
 
         t3 = time.time() - t0
         logging.debug('Time of extraction: %6.3f sec' % (t3))
